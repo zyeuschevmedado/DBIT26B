67e2dd008a8e6ecf13e5ca1537657eb880a22741
zyeuschevmedado <zyeusm@gmail.com>
2026-04-17 00:40:20 +0800

    added register form edit

--- src/dbit26/RegisterForm.java
+++ src/dbit26/RegisterForm.java
@@ -66,6 +66,7 @@ public class RegisterForm extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
